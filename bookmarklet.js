42d6a720a77f3a3348dfb98cf93b504c363e4539
Jason Rosenbaum <seminal@theseminal.com>
2014-08-20 16:06:03 -0400

    Revert "escaping : in IDs because salesforce"
    This reverts commit 88a586317059615cfa087d720d5576be1d432892.
    
    Conflicts:
    	bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -134,34 +134,23 @@ window.Utils || (Utils = {});
 
     var $el = $(el).eq(0),
         $root = $el.parents().eq($el.parents().length - 1),
-        id = $el.attr('id').replace(':','\:');
+        id = $el.attr('id'),
         classList = $el.attr('class') ? $el.attr('class').split(' ').filter(function(el){ return el !== '';}) : [],
-        joinedClassList = classList.join('.').replace(':','\:'),
-        action = $el.attr('action').replace(':','\:'),
-        name = $el.attr('name').replace(':','\:'),
+        joinedClassList = classList.join('.'),
+        action = $el.attr('action'),
+        name = $el.attr('name'),
         value = $el.attr('value'),
         type = $el.attr('type'),
         height = $el.attr('height'),
         width = $el.attr('width'),
         tagName = $el[0].tagName.toLowerCase(),
         form = $el.parents('form'),
+        formId = form.attr('id'),
         formSelector = u.getSelector(form, depth+1),
         classedParent = $el.parents('[class]').eq(0),
         classedParentSelector = u.getSelector(classedParent, depth+1),
         iddParent = $el.parents('[id]').eq(0),
         iddParentSelector = u.getSelector(iddParent, depth+1);
-        
-    if (form.attr('id')) {
-	    var formId = form.attr('id').replace(':','\:');
-    } else {
-	    var formId = form.attr('id');
-    }
-    
-    //console.log(formID);
-    console.log(id);
-    console.log(joinedClassList);
-    console.log(action);
-    console.log(name);
 
     var possibleSelectors = [];
     id && possibleSelectors.push(u.render('#{{ id }}', {
